7f670e99d2f4d495b810ae70bf3bfa53abfea597
traszka116 <80736943+traszka116@users.noreply.github.com>
2024-07-11 06:29:31 +0200

    fix to http.zig file

--- src/http/http.zig
+++ src/http/http.zig
@@ -5,7 +5,6 @@ pub const types = @import("types.zig");
 pub const router = @import("router.zig");
 pub const Route = router.Route;
 pub const Router = router.Router;
-pub const toRoute = router.intoRoute;
 pub const Handler = router.Handler;
 
 pub const Server = @import("server.zig").Server;
